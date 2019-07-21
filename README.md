34902cbfb44d7ccaf1410772d5487dc558bf718b
andrew <andrewjthomsen@yahoo.com>
2019-07-20 22:28:17 -0700

    Customizing HTTP request to automatically parse JSON objects

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Node-Weather-Web-App
-Node-based Asynchronous weather application
+## Description 
+A Node-based Asynchronous weather application that returns current weather conditions by making HTTP requests. Utilizes Request NPM package to make HTTP request. 
+
+## Installation
+- To run this application, download git repository and open application in code editor of choice. Next run `npm install` to download application's dependencies. 
+
--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const url = 'https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/3
 // Two arguments passed into request. 
 // Options object (url) and a function to run once we have the weather data or when we don't bring back the data
 // Func has two arguments: error if no data returns, and response.
+// Added third argument to customize request. 
+// json can be set to true to have request module automatically parse JSON objects
 
-request({ url: url }, (error, response) => {
+request({ url: url, json: true }, (error, response) => {
     const data = JSON.parse(response.body)
+    // data.currently property returns current forecast info
     console.log(data.currently)
 })
