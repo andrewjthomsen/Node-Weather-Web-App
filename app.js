388673f7e0ff3ff374493934c79197e8442cf802
andrew <andrewjthomsen@yahoo.com>
2019-07-20 22:43:09 -0700

    Completed first test challenge for the application. Which was to print a forecast to the user.

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const url = 'https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/3
 // json can be set to true to have request module automatically parse JSON objects
 
 request({ url: url, json: true }, (error, response) => {
-    const data = JSON.parse(response.body)
-    // data.currently property returns current forecast info
-    console.log(data.currently)
+    // console.log(response.body.currently)
+    // Challenge- print a small forecast to the user
+// 1. print "it is currently 58.55 degrees out.
+// There is a 0% chance of rain."
+// Test work!
+    console.log('It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
+
+
