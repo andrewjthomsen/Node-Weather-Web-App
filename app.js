c19f9e95fa764edacbc00bc886c8ee2f5bbebce4
andrew <andrewjthomsen@yahoo.com>
2019-07-21 14:48:44 -0700

    Further built out app.js file to account for user input for location

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
+// Variable to hold onto loaction
+const address = process.argv[2];
 
-
-// Callback chaining
-geocode("San Diego", (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  console.log("Please provide an address.");
+} else {
+  // Callback chaining
+  geocode(address, (error, data) => {
     if (error) {
       return console.log(error);
     }
 
-    console.log(data.location);
-    console.log(forecastData);
-  });
-});
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
 
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
+console.log(process.argv);
