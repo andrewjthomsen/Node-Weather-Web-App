8c38246bc52ffe50db0fc47c8f46b988a8480975
andrew <andrewjthomsen@yahoo.com>
2019-07-20 23:32:10 -0700

    Integrated a second API request using mapbox's geocoding to access latitude and longitude. For now, I'm just printing the result.

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const request = require('request')
+const request = require("request");
 
-const url = 'https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/37.8267,-122.4233'
-
-// HTTP REQUEST
-// Two arguments passed into request. 
-// Options object (url) and a function to run once we have the weather data or when we don't bring back the data
-// Func has two arguments: error if no data returns, and response.
-// Added third argument to customize request. 
-// json can be set to true to have request module automatically parse JSON objects
+const url =
+  "https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/37.8267,-122.4233";
 
 request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body.currently)
-    // Challenge- print a small forecast to the user
-// 1. print "it is currently 58.55 degrees out.
-// There is a 0% chance of rain."
-// Test work!
-    console.log('It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-})
+  // console.log(response.body.currently)
+  console.log(
+    response.body.daily.data[0].summary +
+      " It is currently " +
+      response.body.currently.temperature +
+      " degrees out. There is a " +
+      response.body.currently.precipProbability +
+      "% chance of rain."
+  );
+});
 
+// Geocoding
+// Address -> Lat/Long -> Weather
+const geoCodingUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3anRob21zZW4iLCJhIjoiY2pyNXFjam03MjlnNzQ0c2VzNjIzcWdhdyJ9.OgdsY8LjrFyxmcmOYXuAoA";
 
+request({ url: geoCodingUrl, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+// Prints latitude and longitude
+  console.log(latitude, longitude);
+});
