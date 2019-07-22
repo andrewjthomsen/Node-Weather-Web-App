33ae29e7d8581b0884cbc57bb5364ce96054f98b
andrew <andrewjthomsen@yahoo.com>
2019-07-21 22:39:30 -0700

    Utilized object destructuring in app.js, geocode.js, and forecast.js to rewrite code to become slightly more efficient.

--- utils/forecast.js
+++ utils/forecast.js
@@ -6,17 +6,17 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url =
   "https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/" + latitude + "," + longitude;
-    request({url: url, json: true }, (error, response) => {
+    request({url, json: true }, (error, { body }) => {
         if(error) {
             callback("Unable to connect to weather service!", undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback("Unable to find location. Try searching again... ", undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary +
+            callback(undefined, body.daily.data[0].summary +
                         " It is currently " +
-                        response.body.currently.temperature +
+                        body.currently.temperature +
                         " degrees out. There is a " +
-                        response.body.currently.precipProbability +
+                        body.currently.precipProbability +
                         "% chance of rain.")
         }
     })
